0c4ee89beab12ff25f6f53e36dc2b96523e4d7e4
guocq <guocqmail@163.com>
2017-01-06 19:10:31 +0800

    测试abort

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask
+from flask import request
+from flask import make_response
+from flask import abort
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'hello wordl~'
 
+
 @app.route('/name/<name>')
 def user(name):
-    return 'hello %s' %(name)
+    # user_agent = request.headers.get('User-Agent')
+    request = make_response('xxx')
+    request.set_cookie('answer', '24')
+    return 'hello %s usr-agent ;request_headers %s' % (name,request.headers)
+
+
+@app.route('/ab')
+def get_user():
+    abort(404)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
